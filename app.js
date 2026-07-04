3d143d82a3811f64624c9dd8676f1d0c2047fa14
casingalaaron <casingalaaron04@gmail.com>
2026-07-04 23:20:35 +0800

    implement small portion of function of button particularly the plus and equal

--- app.js
+++ app.js
@@ -1,15 +1,82 @@
-function add(num1, num2){
-    return num1 + num2
+const add = ((currentTotal, inputNumber) => currentTotal += inputNumber)
+const subtract = ((currentTotal, inputNumber) => currentTotal -= inputNumber)
+const multiply = ((currentTotal, inputNumber) => currentTotal *= inputNumber)
+const divide = ((currentTotal, inputNumber) => currentTotal /= inputNumber)
+function operate(operator, num1, num2){
+    switch(operator){
+        case '+':
+            return add(num1, num2)
+            break;
+        case '-':
+            return subtract(num1, num2)
+            break;
+        case '*':
+            return multiply(num1, num2)
+            break;
+        case '÷':
+            return divide(num1, num2)
+            break;
+    }
 }
-function subtract(){
 
-}
-function multiply(){
+const buttons = document.querySelectorAll('button')
+let inputHolder = ""
+let currentBtnClick = ""
+let total = 0
+let currentTotal = 0
+let result = document.querySelector('#result')
+let operator = ""
 
-}
-function divide(){
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        currentBtnClick = button.dataset.button
+        inputHolder += button.dataset.button
+        result.textContent = inputHolder
 
-}
-function operator(){
 
-}
+        switch(currentBtnClick){
+            case '+':
+                result.textContent = currentBtnClick
+                operator = currentBtnClick
+                currentTotal += parseInt(inputHolder)
+                inputHolder = ""
+                break;
+            case '-':
+                result.textContent = currentBtnClick
+                operator = currentBtnClick
+                inputHolder = ""
+                break;
+            case '*':
+                operator = button.dataset.button
+                result.textContent = "\u002A"
+                inputHolder = ""
+                break;
+            case '/':
+                operator = button.dataset.button
+                result.textContent = "\u00F7"
+                inputHolder = ""
+                break;
+            case '=':
+                total = operate(operator, parseInt(currentTotal), parseInt(inputHolder))
+                result.textContent = total
+                inputHolder = total
+                break;
+            case 'del':
+                operator = button.dataset.button
+                
+                result.textContent = "+"
+                inputHolder = ""
+                break;
+            case '%':
+                operator = button.dataset.button
+                result.textContent = "+"
+                inputHolder = ""
+                break;
+            case 'ac':
+                operator = button.dataset.button
+                result.textContent = "+"
+                inputHolder = ""
+                break;
+        }
+    })
+})
